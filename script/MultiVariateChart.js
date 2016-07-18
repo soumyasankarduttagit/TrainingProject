67868d16e76ebb8e0a0c9064bb802848d7b3fcbc
SoumyaSankarDutta <soumyasankardutta@gmail.com>
2016-07-18 13:08:51 +0530

    null value tackled

--- script/MultiVariateChart.js
+++ script/MultiVariateChart.js
@@ -426,12 +426,24 @@ function svgCreate(chartheight,chartwidth,numOfXTick,xLabels,yTickDetails,yTitle
 				}
 				//Drawing Data-plot anchors
 				var plotCircles =drawPlot(xCordArr,yCordarr,plotSliceCollection,plotData);
-				//console.log(plotCircles.length);
+				console.log(plotCircles.length);
 				var prevX=0;
 				var prevY=0;
 				for(var c =0;c<plotCircles.length;c++)
-				{ 
-					
+				{
+					 
+                    
+					var plotCircle = document.createElementNS(NS,"circle");
+					plotCircle.setAttributeNS(null, "cx", plotCircles[c].x);
+					plotCircle.setAttributeNS(null, "cy", plotCircles[c].y);
+					plotCircle.setAttributeNS(null, "r",  width/100);
+					plotCircle.setAttributeNS(null, "fill", "green");
+					var toolTip = document.createElementNS(NS, "title"); 
+                    toolTip.setAttributeNS(null, "class", "plotToolTip"); 
+                    toolTip.innerHTML =plotCircles[c].yValue; 
+                    plotCircle.classList.add("plotDots");
+                    plotCircle.appendChild(toolTip); 
+					svg.appendChild(plotCircle);
 					if((prevX!=0)&&(prevY!=0))
 					{
 						var linkLine = document.createElementNS(NS,"line");
@@ -444,24 +456,9 @@ function svgCreate(chartheight,chartwidth,numOfXTick,xLabels,yTickDetails,yTitle
 						linkLine.classList.add("plotLines");
 						svg.appendChild(linkLine);
 					}
-					
 					prevX=plotCircles[c].x;
 					prevY= plotCircles[c].y;
 					
-				}
-				for(var c =0;c<plotCircles.length;c++)
-				{ 
-					var plotCircle = document.createElementNS(NS,"circle");
-					plotCircle.setAttributeNS(null, "cx", plotCircles[c].x);
-					plotCircle.setAttributeNS(null, "cy", plotCircles[c].y);
-					plotCircle.setAttributeNS(null, "r",  width/100);
-					plotCircle.setAttributeNS(null, "fill", "green");
-					var toolTip = document.createElementNS(NS, "title"); 
-                    toolTip.setAttributeNS(null, "class", "plotToolTip"); 
-                    toolTip.innerHTML =plotCircles[c].yValue; 
-                    plotCircle.setAttributeNS(null,"class","plotDots");
-                    plotCircle.appendChild(toolTip);
-                    svg.appendChild(plotCircle);
 				}
 				div.appendChild(svg);
 				document.body.appendChild(div);
